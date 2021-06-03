1367deb5721349c54785a06662c1f9e994651710
rooler <roel.wier@gmail.com>
2021-06-03 14:53:05 -0400

    3 qubit circuit

--- oranssi/tests/test_fixtures.py
+++ oranssi/tests/test_fixtures.py
@@ -22,6 +22,28 @@ def circuit_1():
 
 
 @pytest.fixture
+def circuit_2():
+    device = qml.device('default.qubit', wires=3)
+
+    def circuit(params, **kwargs):
+        qml.Hadamard(wires=0)
+        qml.Hadamard(wires=1)
+        qml.Hadamard(wires=2)
+        qml.RZ(params[0], wires=0)
+        qml.RZ(params[0], wires=1)
+        qml.RZ(params[0], wires=2)
+        qml.CNOT(wires=[0, 1])
+        qml.CNOT(wires=[1, 2])
+        qml.RZ(params[1], wires=0)
+        qml.RZ(params[1], wires=1)
+        qml.RZ(params[1], wires=2)
+        return qml.state()
+
+    param_shape = (2,)
+    return circuit, device, param_shape
+
+
+@pytest.fixture
 def circuit_1_bad_return_types(request):
     if request.param == 'float':
         def circuit():
