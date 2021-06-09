1d137700fc3d89f77d32273cf1cf3fabdcbe36b8
rooler <roel.wier@gmail.com>
2021-06-09 14:11:45 -0400

    Add docstrings

--- oranssi/plot_utils.py
+++ oranssi/plot_utils.py
@@ -6,11 +6,37 @@ plt.rc('font', family='serif')
 
 
 def spherical_to_state(phi, theta):
-    phi = np.array([np.cos(theta / 2), np.exp(1j * phi) * np.sin(theta / 2)])
-    return np.outer(phi, phi.conj())
+    """
+    Create single qubit state living on the Bloch sphere from the angles `phi` and `theta`
+
+    Args:
+        phi: Polar angle on Bloch sphere
+        theta: Azimuthal angle on Bloch sphere
+
+    Returns:
+        2x2 density matrix array
+
+    """
+    psi = np.array([np.cos(theta / 2), np.exp(1j * phi) * np.sin(theta / 2)])
+    return np.outer(psi, psi.conj())
 
 
 def plot_bloch_sphere_2d(phi: np.ndarray, theta: np.ndarray, costs: np.ndarray, ax, fig):
+    """
+    Given phi and theta, which describe points on a sphere of radius one, plot the cost landscape
+    on the sphere
+
+    Args:
+        phi: Vector with `K` polar angles
+        theta: Vector with `L` azimuthal angles
+        costs: K x L matrix with costs at points (phi_k, theta_l)
+        ax: Matplotlib axes
+        fig: Matplotlib figure
+
+    Returns:
+        inplace
+
+    """
     im = ax.contourf(phi, theta, costs, cmap='Blues')
     ax.set_xlabel(r'$\phi$')
     ax.set_ylabel(r'$\theta$')
@@ -18,6 +44,16 @@ def plot_bloch_sphere_2d(phi: np.ndarray, theta: np.ndarray, costs: np.ndarray, 
 
 
 def state_to_spherical(rho):
+    """
+    Returns the coordinates on the Bloch sphere given a quantum state.
+
+    Args:
+        rho: 2x2 density matrix.
+
+    Returns:
+        phi, theta, the polar and azimuthal angles on the Bloch sphere.
+
+    """
     assert np.isclose(np.trace(rho), 1), '`rho` is not trace 1'
     paulis = get_su_2_operators()
     coeffs = [np.trace(rho @ paulis[i]).real for i in range(3)]
