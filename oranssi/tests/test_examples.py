e192b7a9ff63deb6e22d79bdb32f0a8c57f4625d
rooler <roel.wier@gmail.com>
2021-06-14 11:17:45 -0400

    Add test to verify parameter shift rule

---
+++ oranssi/tests/test_examples.py
@@ -0,0 +1,32 @@
+import pytest
+import pennylane as qml
+import numpy as np
+from oranssi.optimizers import exact_lie_optimizer, local_su_2_lie_optimizer, \
+    local_su_4_lie_optimizer, parameter_shift_optimizer
+from test_fixtures import circuit_1, circuit_2, circuit_1_bad_return_types
+
+
+def test_single_qubit_single_observable_example():
+    nqubits = 1
+    dev = qml.device('default.qubit', wires=nqubits)
+    eta = 0.2
+
+    def circuit(params, **kwargs):
+        # qml.Hadamard(wires=0)
+        qml.RY(params[0], wires=0)
+        # qml.RZ(params[1], wires=0)
+        # return qml.state()
+
+    observables = [qml.PauliX(0)]
+    params = [0.2, ]
+    costs_parameter_shift = parameter_shift_optimizer(circuit, params, observables, dev, eta=eta)
+
+    def circuit(params, **kwargs):
+        # qml.Hadamard(wires=0)
+        qml.RY(params[0], wires=0)
+        # qml.RZ(params[1], wires=0)
+        return qml.state()
+
+    costs_exact = exact_lie_optimizer(circuit, params, observables, dev, eta=eta)
+
+    assert all(np.isclose(x, y) for x, y in zip(costs_parameter_shift, costs_exact))
