91a7ccf6b5334793aa5053a83a583ecd929c1378
rooler <roel.wier@gmail.com>
2021-07-19 13:00:39 -0400

    Added names to su(p) generators

--- oranssi/utils.py
+++ oranssi/utils.py
@@ -1,10 +1,13 @@
 import numpy as np
-from typing import List
 import itertools as it
 import os
 
 
-def get_su_2_operators(identity: bool = False) -> List[np.ndarray]:
+pauli_int_to_str_id = {0: 'I', 1: 'X', 2: 'Y', 3: 'Z'}
+pauli_int_to_str = {0: 'X', 1: 'Y', 2: 'Z'}
+
+
+def get_su_2_operators(identity: bool = False, return_names: bool = False):
     """
     Get the 2x2 SU(2) operators. The dimension of the group is n^2-1, so we have 3 operators.
 
@@ -19,12 +22,20 @@ def get_su_2_operators(identity: bool = False) -> List[np.ndarray]:
     Y = np.array([[0, -1j], [1j, 0]], complex)
     Z = np.array([[1., 0], [0j, -1.]], complex)
     if identity:
-        return [I, X, Y, Z]
+        paulis=  [I, X, Y, Z]
+        if return_names:
+            return paulis, ['I', 'X', 'Y', 'Z']
+        else:
+            return paulis
     else:
-        return [X, Y, Z]
+        paulis = [X, Y, Z]
+        if return_names:
+            return paulis, ['X', 'Y', 'Z']
+        else:
+            return paulis
 
 
-def get_su_4_operators(identity: bool = False) -> List[np.ndarray]:
+def get_su_4_operators(identity: bool = False, return_names: bool = False):
     """
     Get the 4x4 SU(2) operators. The dimension of the group is n^2-1, so we have 15 operators.
 
@@ -43,9 +54,18 @@ def get_su_4_operators(identity: bool = False) -> List[np.ndarray]:
     else:
         paulis = [X, Y, Z]
     operators = []
+    names = []
     for comb in it.product(list(range(len(paulis))), repeat=2):
         operators.append(np.kron(paulis[comb[0]], paulis[comb[1]]))
-    return operators
+        names.append(comb)
+
+    if return_names:
+        if identity:
+            return operators, [''.join([pauli_int_to_str_id[i] for i in n]) for n in names]
+        else:
+            return operators, [''.join([pauli_int_to_str[i] for i in n])  for n in names]
+    else:
+        return operators
 
 
 def operator_2_norm(R: np.ndarray) -> float:
@@ -58,7 +78,7 @@ def operator_2_norm(R: np.ndarray) -> float:
     Returns:
         Scalar corresponding to the norm
     """
-    return np.sqrt(np.trace(R.conjugate().transpose() @ R))
+    return np.sqrt(np.trace(R.conjugate().transpose() @ R)).real
 
 
 def save_path_creator(path, experiment_name):
